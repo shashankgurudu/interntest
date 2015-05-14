1d225c1bb2a19a6b0222d600b862d721fd6e96a0
Shashank <shashank233@gmail.com>
2015-05-14 19:16:35 +0530

    basic calci

---
+++ cal.py
@@ -0,0 +1,11 @@
+a = float(input("enter first number"));
+b = float(input("enter second number"));
+c = raw_input("what do want to do with them + - * /");
+if c == '+':
+	print "sum is ", a+b
+elif c == '-':
+	print "difference is ", a-b;
+elif c == '*':
+	print "multiplication is ", a*b;
+elif c == '/':
+	print "division is ", a/b;
