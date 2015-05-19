3c94a125f9502a85e49f3e08f5de1ed43aee089a
Shashank <shashank233@gmail.com>
2015-05-19 21:48:12 +0530

    call from calci.py

--- calci.py
+++ calci.py
@@ -1,5 +1,10 @@
-import calc
-from calc import *
+#import calc as c
+from numpy import array
+import matrix as m
+from adv import *
+from basic import *
+from files import *
+from sym import *
 a = float(input("enter first number"));
 b = float(input("enter second number"));
 add(a,b)
@@ -9,3 +14,26 @@ div(a,b)
 pow(a,b)
 sqrt(a,b)
 mod(a,b)
+
+
+m1 = input("enter m and n of first array")
+n1 = input()
+m2 = input("enter m and n of second array")
+n2 = input()
+x = []
+y = []
+print "enter elements of first array"
+for i in range(0,int(m1)*int(n1)):
+	x.append(int(input()))
+print array(x).reshape(m1,n1)
+for i in range(0,int(m2)*int(n2)):
+	y.append(int(input()))
+print array(y).reshape(m2,n2)
+
+m.matmul(x,y,m1,n1,m2,n2)
+m.matadd(x,y,m1,n1,m2,n2)
+m.matsub(x,y,m1,n1,m2,n2)
+m.matdiv(x,y,m1,n1,m2,n2)
+
+equa()
+readwrite()
--- sym.py
+++ sym.py
@@ -1,15 +1,16 @@
 from sympy import *
 from sympy.abc import *
-a = input("enter the expression")
-pprint(a)
-print(diff(a,x))
-print(integrate(a,x))
+def equa():
+	a = input("enter the expression")
+	pprint(a)
+	print(diff(a,x))
+	print(integrate(a,x))
 
-b = input("enter expression for summation")
+	b = input("enter expression for summation")
 
-pprint(summation(b,(i,1,10)))
-pprint(solve(a,x))
-pprint(series(sin(x),x))
-expr =a
-expr.subs(x,y)
-pprint(expr)
+	pprint(summation(b,(i,1,10)))
+	pprint(solve(a,x))
+	pprint(series(sin(x),x))
+	expr =a
+	expr.subs(x,y)
+	pprint(expr)
